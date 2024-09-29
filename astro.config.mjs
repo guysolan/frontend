e83dfaf03833d794a0a0fe22e572a2b3040570ed
Guy_Solan <guysolan202@gmail.com>
2024-09-29 12:53:51 +0200

    fixes for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  output: "hybrid",
   integrations: [mdx(), sitemap(), tailwind()],
 });

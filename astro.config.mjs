993a975d671103e4064f1459726e1b666423db17
Guy_Solan <guysolan202@gmail.com>
2024-09-29 13:36:41 +0200

    astro config remove crap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
-import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://guysolan.github.io",
-  base: "/frontend/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [ tailwind()],
   output: "server",
   adapter: vercel(),
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  },
-  vite: {
-    ssr: {
-      noExternal: ['@astrojs/vercel/serverless']
-    }
-  }
 });

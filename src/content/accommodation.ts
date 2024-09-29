f40a0f0205963a7215a6ea8b9146d22202eb2fb4
Guy_Solan <guysolan202@gmail.com>
2024-09-29 12:30:09 +0200

    v1

---
+++ src/content/accommodation.ts
@@ -0,0 +1,82 @@
+import InnOnTheLake from "../assets/accommodation/inn-on-the-lake.jpg";
+import MerryHarriers from "../assets/accommodation/merry-harriers.jpg";
+import LytheHotel from "../assets/accommodation/lythe-hotel.jpg";
+import BarnettHotel from "../assets/accommodation/barnet-hotel.jpg";
+import CrownInn from "../assets/accommodation/crown-inn.jpg";
+import RoseAndCrown from "../assets/accommodation/rose-and-crown.jpg";
+import TheSwan from "../assets/accommodation/the-swan.jpg";
+import TheMulberry from "../assets/accommodation/the-mulberry.jpg";
+import SquirrelInn from "../assets/accommodation/squirrel-inn.webp";
+
+export const accommodation = [
+	{
+		name: "The Inn on the Lake",
+		travel: 6,
+		location: "Godalming",
+		price: "££",
+		image: InnOnTheLake,
+	},
+	{
+		name: "Merry Harriers",
+		travel: 9,
+		location: "Hambledon",
+		price: "££",
+		image: MerryHarriers,
+	},
+	{
+		name: "Barnett Hotel",
+		link: "https://alexanderhotels.co.uk/barnett-hill/",
+		travel: 14,
+		location: "Guildford",
+		price: "£££",
+		image: BarnettHotel,
+	},
+	{
+		name: "The Lythe Hotel",
+		link: "https://lythehill.co.uk/",
+		travel: 21,
+		location: "Haslemere",
+		price: "£££",
+		image: LytheHotel,
+	},
+	{
+		name: "Crown Inn",
+		link: "https://www.crowninnhaslemere.co.uk/",
+		travel: 15,
+		location: "Chiddingfold",
+		price: "££",
+		image: CrownInn,
+	},
+	{
+		name: "Rose and Crown",
+		link: "https://www.roseandcrownhaslemere.co.uk/",
+		travel: 7,
+		location: "Godalming",
+		price: "££",
+		image: RoseAndCrown,
+	},
+	{
+		name: "The Swan",
+		link: "https://www.theswanhaslemere.co.uk/",
+		travel: 15,
+		location: "Chiddingfold",
+		price: "££",
+		image: TheSwan,
+	},
+	{
+		name: "The Mulberry",
+		link: "https://www.themulberryhotel.co.uk/",
+		travel: 18,
+		location: "Haslemere",
+		price: "££",
+		image: TheMulberry,
+	},
+	{
+		name: "Squirrel Inn",
+		link: "https://the-squirrel.co.uk/",
+		travel: 12,
+		location: "Hurtmore",
+		price: "££",
+		image: SquirrelInn,
+	}
+];
